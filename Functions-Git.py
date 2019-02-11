96c80f72f209c96d2d1b15e7adaca7e354858db9
Juan Salcedo <02Juan.Salcedo@gulllakecs.org>
2019-02-11 09:54:20 -0500

    Add a Greetings Function

---
+++ Functions-Git.py
@@ -0,0 +1,16 @@
+# 4.13.3: Greeting
+# Juan Salcedo
+# 2.6.19
+
+# Asking for user input
+name = input("What is your name? ")
+
+# Defineing a variable to print a statement/string
+def greeting():
+    print("Hi there " + name + "! ")
+    print("Nice to meet you")
+
+# Calling in the variable "greeting()"
+greeting()
+
+
